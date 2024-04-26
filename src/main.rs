943c9de177548901dbe82fc12433bc9cc18accb3
Rhian McClelland <rhianmcclelland193@hotmail.com>
2024-04-26 11:10:43 +1000

    Seperate and correct tree & blob

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use rustit::Blob;
+use rustit::objects::blob::Blob;
+use rustit::objects::tree::Tree;
+
 
 fn main() {
-    let mut blob = Blob::try_new("Cargo.toml").expect("Error reading file");
-    blob.save();
+
+    let files = vec!["./src/main.rs", 
+                                "./src/lib.rs", 
+                                "./src/objects.rs",
+                                "./src/hash.rs",
+                                "./src/objects/blob.rs",
+                                "./src/objects/tree.rs",
+                                "./src/hash/rhash.rs"];
+
+    let folders = vec!["./src/objects", "./src/hash"];
+
+    for file in files {
+        let mut blob: Blob = Blob::try_new(file).expect("Error reading file");
+        blob.save();
+    }
+
+    for folder in folders {
+        let mut tree: Tree = Tree::try_new(folder).expect("Error reading file");
+        tree.save();
+    }
+
+    let mut tree: Tree = Tree::try_new("./src").expect("Error reading file");
+    tree.save();
+    
+
 }
---
+++ src/objects/tree.rs
@@ -0,0 +1,87 @@
+use super::blob::Blob;
+use crate::hash::rhash::RHash;
+use core::hash;
+use std::{fs, io::Error};
+
+pub struct Tree {
+    pub entries: Vec<TreeEntry>,
+    pub content: Vec<u8>,
+    pub hash: RHash,
+    pub name: String,
+}
+
+pub enum TreeEntry {
+    Tree(Tree),
+    Blob(Blob),
+}
+
+impl Tree {
+    pub fn try_new(dir_path: &str) -> Result<Self, Error> {
+        let name = dir_path.split("/").last().unwrap().to_string();
+        let entries = Tree::get_objects(dir_path)?;
+        let content = Tree::gen_content(&entries);
+        let hash = Tree::gen_hash(&content)?;
+        Ok(Tree { entries, content,  hash, name })
+    }
+
+    pub fn get_hash(&self) -> &RHash {
+        &self.hash
+    }
+
+    pub fn save(&mut self) -> () {
+        let (first_two, last_38) = self.hash.split_hash();
+        let dir = format!(".rustit/objects/{}", first_two);
+        let file = format!("{}/{}", dir, last_38);
+        fs::create_dir_all(dir).expect("Error creating directory");
+        fs::write(file, &self.content).expect("Error writing file");
+    }
+
+    fn gen_hash( content: &Vec<u8>) -> Result<RHash, Error> {
+        Ok(RHash::new(&content))
+    }
+
+    fn get_objects(dir_path: &str) -> Result<Vec<TreeEntry>, Error> {
+        let path_entries = fs::read_dir(dir_path)?;
+        let mut entries = Vec::new();
+
+        for entry in path_entries {
+            let entry = entry?;
+            let path = entry.path();
+            let metadata = entry.metadata()?;
+
+            if metadata.is_dir() {
+                let tree = Tree::try_new(path.to_str().unwrap())?;
+                entries.push(TreeEntry::Tree(tree));
+            } else {
+                let blob = Blob::try_new(path.to_str().unwrap())?;
+                entries.push(TreeEntry::Blob(blob));
+            }
+        }
+
+        Ok(entries)
+    }
+
+    fn gen_content(entries: &Vec<TreeEntry>) -> Vec<u8> {
+        let mut content = String::new();
+
+        for entry in entries {
+            match entry {
+                TreeEntry::Tree(tree) => {
+                    content.push_str("tree ");
+                    content.push_str(&tree.get_hash().as_string());
+                    content.push_str(format!(" {}\n", &tree.name).as_str());
+                },
+                TreeEntry::Blob(blob) => {
+                    content.push_str("blob ");
+                    content.push_str(&blob.get_hash().as_string());
+                    content.push_str(format!(" {}\n", &blob.get_name()).as_str());
+                },
+            }
+        }
+        
+        let header = format!("tree {}\0", content.len());
+        let data = [header.as_bytes(), content.as_bytes()].concat();
+
+        data
+    }
+}
